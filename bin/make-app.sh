2dc166f90da22cb4c7b37abf9bcc5d9785ac8d18
Fede J. Álvarez <falvarez@bitban.com>
2024-03-07 20:51:48 +0100

    wip: Scripts to make bundle app and dmg

--- bin/make-app.sh
+++ bin/make-app.sh
@@ -1,12 +1,40 @@
 #!/usr/bin/env bash
 
-mkdir build/Babaliba.app
-cp -r templates/app/* build/Babaliba.app/
+# @see https://github.com/AugustoRuiz/UWOL/blob/master/buildOSXApp.sh
 
-cp bennugd/bgdi.app/Contents/MacOS/* build/Babaliba.app/Contents/MacOS/
-cp -r build/babaliba.dcb build/Babaliba.app/Contents/MacOS/
-cp -r src/data build/Babaliba.app/Contents/MacOS/
-cp -r src/gfx build/Babaliba.app/Contents/MacOS/
-cp -r src/sound build/Babaliba.app/Contents/MacOS/
+APPNAME=Babaliba
+APPBUNDLE=build/${APPNAME}.app
 
 
+# Compilamos el juego de cero y sin debug, para no empaquetar lo que no queremos
+
+bin/clean.sh
+bin/compile.sh
+
+# Generamos el bundle
+
+rm -rf ${APPBUNDLE}
+mkdir -p ${APPBUNDLE}/Contents
+cp -r templates/app/* ${APPBUNDLE}/
+cp bennugd/bgdi.app/Contents/MacOS/* ${APPBUNDLE}/Contents/MacOS/
+cp -r build/babaliba.dcb ${APPBUNDLE}/Contents/MacOS/
+cp -r src/data ${APPBUNDLE}/Contents/MacOS/
+cp -r src/gfx ${APPBUNDLE}/Contents/MacOS/
+cp -r src/sound ${APPBUNDLE}/Contents/MacOS/
+
+# Iconos
+
+rm -rf ${APPNAME}.iconset
+mkdir ${APPNAME}.iconset
+sips -z 16 16     assets/${APPNAME}Icon.png --out ${APPNAME}.iconset/icon_16x16.png
+sips -z 32 32     assets/${APPNAME}Icon.png --out ${APPNAME}.iconset/icon_16x16@2x.png
+sips -z 32 32     assets/${APPNAME}Icon.png --out ${APPNAME}.iconset/icon_32x32.png
+sips -z 64 64     assets/${APPNAME}Icon.png --out ${APPNAME}.iconset/icon_32x32@2x.png
+sips -z 128 128   assets/${APPNAME}Icon.png --out ${APPNAME}.iconset/icon_128x128.png
+sips -z 256 256   assets/${APPNAME}Icon.png --out ${APPNAME}.iconset/icon_128x128@2x.png
+sips -z 256 256   assets/${APPNAME}Icon.png --out ${APPNAME}.iconset/icon_256x256.png
+sips -z 512 512   assets/${APPNAME}Icon.png --out ${APPNAME}.iconset/icon_256x256@2x.png
+sips -z 512 512   assets/${APPNAME}Icon.png --out ${APPNAME}.iconset/icon_512x512.png
+cp assets/${APPNAME}Icon.png ${APPNAME}.iconset/icon_512x512@2x.png
+iconutil -c icns -o ${APPBUNDLE}/Contents/Resources/${APPNAME}.icns ${APPNAME}.iconset
+rm -r ${APPNAME}.iconset
